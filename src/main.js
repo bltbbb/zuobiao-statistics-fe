3208c29085254ea5ab31a5c7279bc4f671c6ccdd
guo <guochunyan@shixinyun.com>
2017-06-22 19:48:28 +0800

    [update]框架調整

--- src/main.js
+++ src/main.js
@@ -3,14 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import $ from 'jquery'
-import './assets/css/public/bootstrap.min.css'
-import './assets/js/public/bootstrap.min'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
