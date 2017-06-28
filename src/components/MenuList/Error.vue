1be8c9aac329cd266b98c32f7d9a776983519111
guo <guochunyan@shixinyun.com>
2017-06-28 16:31:54 +0800

    [update]错误报告页面

--- src/components/MenuList/Error.vue
+++ src/components/MenuList/Error.vue
@@ -1,21 +1,226 @@
 <!--错误报告-->
 <template>
   <div class="Error content-box">
-    {{msg}}
+    <div class="title-box">
+      <Calendar @timeValue="getTime"></Calendar>
+      <div class="title-select-box">
+        <el-select v-model="val" placeholder="渠道">
+          <el-option
+            v-for="canal in canals"
+            :key="canal.val"
+            :label="canal.label"
+            :value="canal.val">
+          </el-option>
+        </el-select>
+        <el-select v-model="Eval" placeholder="版本">
+          <el-option
+            v-for="edition in editions"
+            :key="edition.Eval"
+            :label="edition.label"
+            :value="edition.Eval">
+          </el-option>
+        </el-select>
+      </div>
+    </div>
+    <div class="trend-box">
+      <div class="part1">
+      </div>
+      <div id="activeChart" class="chart" :style="{width: '100%', height: '400px'}"></div>
+      <el-radio-group v-model="radio2" class="radio-box">
+        <el-radio :label="1">错误次数</el-radio>
+      </el-radio-group>
+      <!--     <Calendar @timeValue="getTableTime" class="calendar-box"></Calendar>-->
+      <!--表格-->
+      <el-table
+        :data="tableData5"
+        style="width: 100%">
+        <el-table-column type="selection" width="55">
+        </el-table-column>
+        <el-table-column type="expand">
+          <template scope="props">
+            <el-form label-position="left" inline class="demo-table-expand">
+              <el-table :data="tableData5" style="width: 100%">
+                <el-table-column label="错误设备" prop="id">
+                </el-table-column>
+                <el-table-column label="错误次数" prop="name">
+                </el-table-column>
+                <el-table-column label="占比" prop="desc">
+                </el-table-column>
+              </el-table>
+            </el-form>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="错误类型" prop="id">
+          <template scope="scope">
+            <span @click="handleLook(scope.$index, scope.row)">{{ scope.row.id }} </span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="优先级"
+          prop="name">
+        </el-table-column>
+        <el-table-column
+          label="错误次数"
+          prop="desc">
+        </el-table-column>
+        <el-table-column label="操作">
+          <template scope="scope">
+            <el-button
+              size="small"
+              @click="handleLook(scope.$index, scope.row)">查看明细
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage4"
+                     class="radio-box"
+                     :page-sizes="[20, 50, 100]" :page-size="20" layout="total, sizes, prev, pager, next, jumper"
+                     :total="100">
+      </el-pagination>
+    </div>
   </div>
 </template>
 
 <script>
+  // 引入基本模板
+  let echarts = require('echarts/lib/echarts');
+  // 引入柱状图组件
+  require('echarts/lib/chart/line');
+  // 引入提示框和title组件
+  require('echarts/lib/component/tooltip');
+  require('echarts/lib/component/title');
+  require('echarts/lib/component/legend');
+  import Calendar from '@/components/calendar/Calendar'
   export default {
     data() {
       return {
-        msg: 'I am Error'
+        value: '1',
+        canals: [{
+          val: '1',
+          label: '全部渠道'
+        }],
+        val: '1',
+        editions: [{
+          Eval: '1',
+          label: '全部版本'
+        },
+          {
+            Eval: '2',
+            label: '1.4'
+          }],
+        Eval: "1",
+        // 第一部分
+        list: [
+          {id: "1", title: "登录用户", message: 'Foo', number: "8096798"},
+          {id: "2", title: "日活/月活", message: 'Bar', number: "8096798"}
+        ],
+        radio2: 1,
+        // 表格数据
+        tableData5: [{
+          id: '12987122',
+          name: '好滋好味鸡蛋仔',
+          category: '江浙小吃、小吃零食',
+          desc: '荷兰优质淡奶，奶香浓而不腻',
+          address: '上海市普陀区真北路',
+          shop: '王小虎夫妻店',
+          shopId: '10333'
+        }, {
+          id: '12987123',
+          name: '好滋好味鸡蛋仔',
+          category: '江浙小吃、小吃零食',
+          desc: '荷兰优质淡奶，奶香浓而不腻',
+          address: '上海市普陀区真北路',
+          shop: '王小虎夫妻店',
+          shopId: '10333'
+        }, {
+          id: '12987125',
+          name: '好滋好味鸡蛋仔',
+          category: '江浙小吃、小吃零食',
+          desc: '荷兰优质淡奶，奶香浓而不腻',
+          address: '上海市普陀区真北路',
+          shop: '王小虎夫妻店',
+          shopId: '10333'
+        }, {
+          id: '12987126',
+          name: '好滋好味鸡蛋仔',
+          category: '江浙小吃、小吃零食',
+          desc: '荷兰优质淡奶，奶香浓而不腻',
+          address: '上海市普陀区真北路',
+          shop: '王小虎夫妻店',
+          shopId: '10333'
+        }],
+        currentPage4: 4
+      }
+    }
+    ,
+    // 平台图表格-->
+    mounted()
+    {
+      this.drawLine();
+    }
+    ,
+    components: {
+      Calendar
+    },
+    methods: {
+      selected: function (gameName) {
+        this.activeName = gameName
+      }
+      ,
+      // 图表格绘制
+      drawLine()
+      {
+        // 基于准备好的dom，初始化echarts实例
+        let myChart = echarts.init(document.getElementById('activeChart'));
+        // 绘制图表
+        myChart.setOption({
+          title: {text: '全平台注册用户'},
+          tooltip: {
+            trigger: 'axis'
+          },
+          legend: {
+            data: ['今天', '昨天']
+          },
+          xAxis: {
+            data: ["0:00-0:59", "0:00-0:59", "0:00-0:59", "0:00-0:59", "0:00-0:59", "0:00-0:59"]
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [{
+            name: '注册用户',
+            type: 'line',
+            data: [5, 20, 36, 10, 10, 20]
+          }]
+
+        });
+        window.onresize = myChart.resize;
+      },
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+      },
+      //获取日历时间
+      getTime(msg){
+        console.log(msg)
+      },
+      getTableTime(msg){
+        console.log(msg)
+      },
+      //查看明细
+      handleLook(index, row) {
+        console.log(index, row);
       }
     }
   }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="sass">
-
+<style scoped lang="scss">
+  @import '../../assets/css/page/public.scss';
+  @import '../../assets/css/page/error.scss';
 </style>
